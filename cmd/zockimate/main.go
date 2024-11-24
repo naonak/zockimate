d541bc3f4d4c87d979079fa18ab20b6f93e4b358
Naonak <root@truenas.local>
2024-11-24 18:34:54 +0100

    Fix notify

--- cmd/zockimate/main.go
+++ cmd/zockimate/main.go
@@ -171,7 +171,7 @@ Examples:
         },
     }
 
-    cmd.Flags().BoolVarP(&opts.Notify, "notify", "N", false,
+    cmd.Flags().BoolVar(&opts.Notify, "notify", false,
         "Send notifications through Apprise")
     cmd.Flags().BoolVarP(&opts.Force, "force", "f", false,
         "Force update even if no new image available")
@@ -262,7 +262,7 @@ Examples:
         },
     }
 
-    cmd.Flags().BoolVarP(&opts.Notify, "notify", "N", false,
+    cmd.Flags().BoolVar(&opts.Notify, "notify", false,
         "Send notifications through Apprise")
     cmd.Flags().BoolVarP(&opts.Force, "force", "f", false,
         "Force check even with local image")
@@ -520,7 +520,7 @@ func newScheduleUpdateCmd(cfg *config.Config) *cobra.Command {
         },
     }
 
-    cmd.Flags().BoolVarP(&opts.Notify, "notify", "N", true,
+    cmd.Flags().BoolVar(&opts.Notify, "notify", true,
         "Send notifications through Apprise")
     cmd.Flags().BoolVarP(&opts.Force, "force", "f", false,
         "Force update even if no new image available")
@@ -570,7 +570,7 @@ func newScheduleCheckCmd(cfg *config.Config) *cobra.Command {
         },
     }
 
-    cmd.Flags().BoolVarP(&opts.Notify, "notify", "N", true,
+    cmd.Flags().BoolVar(&opts.Notify, "notify", true,
         "Send notifications through Apprise")
     cmd.Flags().BoolVarP(&opts.Force, "force", "f", false,
         "Force check even with local image")
